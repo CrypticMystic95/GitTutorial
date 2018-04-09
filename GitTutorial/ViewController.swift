b94ff7c9ce206670eccf401c007d76cb7681ef9e
Raul Lopez <areacode.code@gmail.com>
2018-04-09 14:32:47 -0700

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Raul on 4/9/18.
+//  Copyright © 2018 Monkey Wrench Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
